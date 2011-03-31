c33ba9f4e2cc814cbef6dc60677135723f91693d
jankuca <jan@jankuca.com>
2011-03-31 16:01:26 +0200

    add JSON library; remove utils

--- flowoff.js
+++ flowoff.js
@@ -3,13 +3,11 @@ window.FlowOff = {
 		var old_root = require.ROOT;
 		require.ROOT += path;
 		require.js(
-			'lib/utils/utils.js',
 			'lib/json/json2.js',
 			'lib/lang.js/lang.js',
 			'lib/ejs/ejs.js',
 			'lib/mongo2sql/mongo2sql.js',
 			'lib/uuid/uuid.js',
-			'src/modules/utils.js',
 			'src/modules/router.js',
 			'src/modules/operation.js',
 			'src/modules/model.js',
