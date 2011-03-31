5ad5187bb6a4a53c58ca56751a94017da9ebf1df
jankuca <jan@jankuca.com>
2011-03-31 14:49:30 +0200

    add loader

---
+++ flowoff.js
@@ -0,0 +1,24 @@
+window.FlowOff = {
+	'load': function (path, callback) {
+		var old_root = require.ROOT;
+		require.ROOT += path;
+		require.js(
+			'lib/utils/utils.js',
+			'lib/json/json2.js',
+			'lib/lang.js/lang.js',
+			'lib/ejs/ejs.js',
+			'lib/mongo2sql/mongo2sql.js',
+			'lib/uuid/uuid.js',
+			'src/modules/utils.js',
+			'src/modules/router.js',
+			'src/modules/operation.js',
+			'src/modules/model.js',
+			'src/modules/controller.js',
+			'src/modules/component.js',
+			'src/modules/form.js',
+			'src/flowoff.js',
+			callback
+		);
+		require.ROOT = old_root;
+	},
+};
