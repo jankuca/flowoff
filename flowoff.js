02eb7eb7401d01a88573f7588221d30d58b42dfb
jankuca <jan@jankuca.com>
2011-06-11 17:57:06 +0200

    add utils lib

--- flowoff.js
+++ flowoff.js
@@ -2,21 +2,30 @@ window.FlowOff = {
 	'load': function (path, callback) {
 		var old_root = require.ROOT;
 		require.ROOT += path;
-		require.js(
-			'lib/json/json2.js',
-			'lib/lang.js/lang.js',
-			'lib/ejs/ejs.js',
-			'lib/mongo2sql/mongo2sql.js',
-			'lib/uuid/uuid.js',
-			'src/modules/router.js',
-			'src/modules/operation.js',
-			'src/modules/model.js',
-			'src/modules/controller.js',
-			'src/modules/component.js',
-			'src/modules/form.js',
-			'src/flowoff.js',
-			callback
-		);
-		require.ROOT = old_root;
+
+		var _load = function () {
+			require.js(
+				'lib/json/json2.js',
+				'lib/lang.js/lang.js',
+				'lib/ejs/ejs.js',
+				'lib/mongo2sql/mongo2sql.js',
+				'lib/uuid/uuid.js',
+				'src/modules/router.js',
+				'src/modules/operation.js',
+				'src/modules/model.js',
+				'src/modules/controller.js',
+				'src/modules/component.js',
+				'src/modules/form.js',
+				'src/flowoff.js',
+				callback
+			);
+			require.ROOT = old_root;
+		}
+
+		if (Function.prototype.inherit === void 0) {
+			require.js('lib/utils/utils.js', _load);
+		} else {
+			_load();
+		}
 	},
 };
